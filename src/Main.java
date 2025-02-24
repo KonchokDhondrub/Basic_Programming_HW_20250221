140c6d1b382ca6f6b3eff226f3719658781062ad
kd <erm.melnik@gmail.com>
2025-02-24 10:59:18 +0100

    Rebuilding code. Added universal methods.

--- src/Main.java
+++ src/Main.java
@@ -18,29 +18,23 @@ public class Main {
         List<Person> team2 = List.of(new Person("Karl", 33), new Person("Robert", 39),
                 new Person("Jack", 22),   new Person("Simon", 44), new Person("Elena", 45));
 
-        Set<Person> allEmployees = new HashSet<>();
-        allEmployees.addAll(team1);
-        allEmployees.addAll(team2);
         System.out.println("\tList 1: " + team1 + "\n\tList 2: " + team2);
-        System.out.println("\tResult: " + allEmployees);
+        System.out.println("\tResult: " + allEmployees(team1, team2));
 
         // Task 3
         System.out.println("\nTask 3 - Get List of not duplicated objects from String");
         String listOfNames = "Jack,John,Nick,John";
         System.out.println("\tString: [" + listOfNames + ']');
-        System.out.println("\tResult: " + removeDuplicates(listOfNames));
+        removeDuplicates(listOfNames);
 
         // Task 4
         System.out.println("\nTask 4 - Get Set of duplicated Persons from both lists");
         List<Person> list1 = List.of(new Person("Alice", 30), new Person("Jack", 25), new Person("John", 35));
         List<Person> list2 = List.of(new Person("Jack", 25), new Person("John", 35), new Person("David", 40));
-        Set<Person> setList1 = new HashSet<>(list1);
-        Set<Person> setList2 = new HashSet<>(list2);
-        setList1.retainAll(setList2);
 
         System.out.println("\tList 1: " + list1);
         System.out.println("\tList 2: " + list2);
-        System.out.println("\tResult: " + setList1);
+        System.out.println("\tResult: " + retailAll(list1, list2));
 
         // Task 5
         System.out.println("\nTask 5 - Get Set of duplicated Persons");
@@ -49,6 +43,7 @@ public class Main {
         System.out.println("\t" + listOfPersons);
         System.out.println("\tResult: " + findDuplicates(listOfPersons));
     }
+
 
     // Task 1
     public static <T> boolean duplicates(List<T> list) {
@@ -61,10 +56,27 @@ public class Main {
         return false;
     }
 
+    // Task 2
+    public static <T> List<T> allEmployees(List<T> l1, List<T> l2){
+        Set<T> allEmployees = new HashSet<>();
+        allEmployees.addAll(l1);
+        allEmployees.addAll(l2);
+        return new ArrayList<>(allEmployees);
+    }
+
     // Task 3
-    public static Set<String> removeDuplicates(String listOfNames) {
+    public static void removeDuplicates(String listOfNames) {
         String[] namesArray = listOfNames.split(",");
-        return new HashSet<>(Arrays.asList(namesArray));
+        for (String name : namesArray) {
+            System.out.println("\t" + name);
+        }
+    }
+
+    // Task 4
+    public static <T> List<T> retailAll(List<T> l1, List<T> l2){
+        HashSet<T> setList1 = new HashSet<>(l1);
+        setList1.retainAll(l2);
+        return new ArrayList<>(setList1);
     }
 
     // Task 5
